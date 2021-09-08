973d653e122cb1f300a26787b7f36a96fda78124
Enderhoang <61771609+Enderhoang@users.noreply.github.com>
2021-09-08 21:03:22 +0700

    eval simple math symbol conversion

--- commands/eval.js
+++ commands/eval.js
@@ -2,18 +2,27 @@ const { SlashCommandBuilder } = require('@discordjs/builders')
 const BigEval = require('bigeval')
 var Math = new BigEval()
 
+
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('eval')
-		.setDescription('Evaluate an expression and return the result')
-    .addStringOption(option => 
-      option.setName('expression')
-            .setDescription('The expression to evaluate [string]')
-            .setRequired(true)
+  data: new SlashCommandBuilder()
+  .setName('eval')
+  .setDescription('Evaluate an expression and return the result')
+  .addStringOption(option => 
+    option.setName('expression')
+    .setDescription('The expression to evaluate [string]')
+    .setRequired(true)
     ),
-	async execute(interaction) {
-    const expression = interaction.options.getString('expression');
-    await interaction.reply({ content: `${expression} = ${Math.exec(expression)}` })
-    console.log(expression)
+    async execute(interaction) {
+    symbols = ['π']
+    symvalue = ['3.14']
+
+    var expression = interaction.options.getString('expression')
+    var expression_2 = expression
+    symbols.forEach(value => {
+      expression_2 = expression_2.replace(value, symvalue[symbols.indexOf(value)])
+    })
+
+    await interaction.reply({ content: `${expression} = ${Math.exec(expression_2)}` })
+    console.log(expression + ' = ' + expression_2)
 	}
 }
