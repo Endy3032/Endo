4b07dcf743a4944f09744050aae5ba3005e4bd08
Enderhoang <61771609+Enderhoang@users.noreply.github.com>
2021-09-11 01:14:01 +0700

    added back logging

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,10 +1,10 @@
 module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
-		// interaction.isCommand() ? console.log(`[${interaction.user.tag} - #${interaction.channel.name}] - Triggered the [${interaction.commandName}] command`)
-    // : interaction.isButton() ? console.log(`[${interaction.user.tag} - #${interaction.channel.name}] - Pushed the [${interaction.message.interaction.commandName}]'s command [${interaction.customId}] button`)
-    // : interaction.isSelectMenu() ? console.log(`[${interaction.user.tag} - #${interaction.channel.name}] - Chose the [${interaction.message.interaction.commandName}] command's [${interaction.values[0]}] option`)
-    // : console.log(interaction + '\nTHIS INTERACTION IS NOT RECORDED PLEASE DO IT IMMEDIATELY')
+		interaction.isCommand() ? console.log(`[${interaction.user.tag} - #${interaction.channel.name}] - Triggered the [${interaction.commandName}] command`)
+    : interaction.isButton() ? console.log(`[${interaction.user.tag} - #${interaction.channel.name}] - Pushed the [${interaction.message.interaction.commandName}]'s command [${interaction.customId}] button`)
+    : interaction.isSelectMenu() ? console.log(`[${interaction.user.tag} - #${interaction.channel.name}] - Chose the [${interaction.message.interaction.commandName}] command's [${interaction.values[0]}] option`)
+    : console.log(interaction + '\nTHIS INTERACTION IS NOT RECORDED PLEASE DO IT IMMEDIATELY')
 
     if (interaction.isCommand()) {
       commandName = interaction.commandName
