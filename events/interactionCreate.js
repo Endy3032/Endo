75169cec8cd536cc3feba7ecd4b9eef531b5496f
Enderhoang <61771609+Enderhoang@users.noreply.github.com>
2021-09-11 17:18:41 +0700

    added bot logging function

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,19 +1,27 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+var logStream = fs.createWriteStream('./botlog.log', {flags: 'a'});
+dotenv.config()
+
+
 module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
-    // if (interaction.guildId !== '864972641219248140') {return}
-    console.log(interaction)
+    // if (interaction.guildId !== process.env.GUILD) {return}
+    // console.log(interaction)
 
+    message = `[${new Date().toLocaleString('default', {dateStyle: 'short', timeStyle: 'medium', hour12: false})} | ${interaction.user.tag} - #`
     interaction.guildId
-    ? message = `[${interaction.user.tag} - #${interaction.channel.name}] - `
-    : message = `[${interaction.user.tag} - DM] - `
+    ? message += `${interaction.channel.name}] - `
+    : message += `DM] - `
 
 		interaction.isCommand() ? message += `Triggered the [${interaction.commandName}] command`
     : interaction.isButton() ? message += `Pushed the [${interaction.message.interaction.commandName}]'s command [${interaction.customId}] button`
     : interaction.isSelectMenu() ? message += `Chose the [${interaction.customId.slice(0, -5)}] command's [${interaction.values[0]}] option`
-    : console.log(interaction + '\nTHIS INTERACTION IS NOT RECORDED PLEASE DO IT IMMEDIATELY')
+    : (console.log(interaction), message += 'THIS INTERACTION IS NOT RECORDED PLEASE DO IT IMMEDIATELY')
 
-    // console.log(message)
+    console.log(message)
+    logStream.write(message + '\n')
 
     let commandName
     if (interaction.isCommand()) {
