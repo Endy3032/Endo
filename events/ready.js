bada85a5c5cd1d6b4aa93fb502745671bfc810dd
Enderhoang <61771609+Enderhoang@users.noreply.github.com>
2021-09-11 10:16:06 +0700

    deleted weird characters in deploy scripts

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,17 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
 	name: 'ready',
 	once: true,
-	execute(client) {
-		console.log(`[${client.user.tag}][SYSTEM] Ready`);
+	async execute(client) {
+		console.log(`[${client.user.tag}][SYSTEM] Ready`)
 
     client.user.setPresence({
       activities: [{ name: '🌧agoraphobic🌧', type: 2 }],
       status: 'idle'
-    });
+    })
+
+    channel = client.channels.cache.get("769497610300948480");
+    channel.send({content: '`[NOTIFICATION]` System Online'})
 	},
 };
