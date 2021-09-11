ad192a724416839c89ad303adfd96d3483f52464
Enderhoang <61771609+Enderhoang@users.noreply.github.com>
2021-09-11 11:58:04 +0700

    changed var to const for deployguild

--- other/deployGuild.js
+++ other/deployGuild.js
@@ -12,7 +12,7 @@ const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 commandFiles.forEach(command => {
-  var cmd = require(`../commands/${command}`);
+  const cmd = require(`../commands/${command}`);
   commands.push(cmd.data.toJSON());
 });
 
