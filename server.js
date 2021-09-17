189ae88b09d59a5a1b9e394a8bbd7800c62d3f80
Hoang Nguyen <61771609+Endy3032@users.noreply.github.com>
2021-09-17 05:46:34 +0000

    basic server again

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+const fs = require('fs');
 const express = require('express')
-
+const morgan = require('morgan')
 const server = express()
+const port = 3032
+
+var httplog = fs.createWriteStream('./http.log', {flags: 'a'});
+server.use(morgan('[:date[web]] ":method :url" :status :user-agent', { stream: httplog }))
+
 server.all('/', (req, res)=>{
-    res.send('Your bot is alive!')
+  res.send(`Website coming in the future\nStay tuned`)
 })
 
 function keepAlive(){
-  server.listen(3000, ()=>{console.log("Server is Ready!")});
+  server.listen(port, () => {
+    console.log(`Server is Ready!`)
+  });
 }
 
 module.exports = keepAlive;
