796769a36b81de2efcd4760864efda25dfbab461
Enderhoang <61771609+Enderhoang@users.noreply.github.com>
2022-02-21 22:07:01 +0700

    improved interaction handling

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,24 +1,21 @@
-const index = require("../index.js")
-const { rep } = require("../other/misc.js")
-const dotenv = require("dotenv")
 const os = require("os")
+const dotenv = require("dotenv")
+const index = require("../index.js")
+const { emojis, rep } = require("../other/misc.js")
 dotenv.config()
 
 module.exports = {
   name: "interactionCreate",
   async execute(interaction) {
-    if (os.hostname().indexOf("local") > -1) {
-      if (interaction.guildId !== process.env.GUILD) {return}
-    } else {
-      if (interaction.guildId == process.env.GUILD) {return}
+    if ((os.hostname().includes("local") && interaction.guildId !== process.env.GUILD) || (interaction.guildId == process.env.GUILD && !os.hostname().includes("local"))) {
+      return
     }
 
     commandName =
     interaction.isChatInputCommand() || interaction.isAutocomplete() ? interaction.commandName
-      : interaction.isButton() ? interaction.message.interaction.commandName || interaction.message.interaction.name
-        : interaction.isSelectMenu() ? interaction.customId.slice(0, -5)
-          : interaction.isContextMenuCommand() ? interaction.commandName.replace("[G] ", "")
-            : null
+      : interaction.isButton() || interaction.isSelectMenu() ? interaction.message.interaction.commandName || interaction.message.interaction.name
+        : interaction.isContextMenuCommand() ? interaction.commandName.replace("[G] ", "")
+          : null
 
     if (!interaction.isAutocomplete()) {
       message = `[${interaction.user.tag} - `
@@ -31,7 +28,7 @@ module.exports = {
       interaction.isChatInputCommand() && interaction.options._group ? `Ran the [${commandName}:${interaction.options._group}:${interaction.options._subcommand}] command`
         : interaction.isChatInputCommand() && interaction.options._subcommand ? `Ran the [${commandName}:${interaction.options._subcommand}] command`
           : interaction.isChatInputCommand() ? `Ran the [${commandName}] command`
-            : interaction.isButton() ? `Pushed the [${commandName}]'s command [${interaction.customId}] button`
+            : interaction.isButton() ? `Pushed the [${commandName}] command's [${interaction.customId}] button`
               : interaction.isSelectMenu() ? `Chose the [${commandName}] command's [${interaction.values[0]}] option`
                 : interaction.isContextMenuCommand() ? `Ran the [${commandName}] context menu command`
                   : (console.log(interaction), "This interaction type hasn't been logged yet. <@554680253876928512>")
@@ -41,44 +38,42 @@ module.exports = {
 
     const command = interaction.client.commands.get(commandName)
 
-    if (interaction.isChatInputCommand()) {
-      try {await command.execute(interaction)}
-      catch (err) {
-        console.error(err)
-        rep(interaction, { content: "This interaction failed (without the red coloring :D) [Command Error]", ephemeral: true })
-      }
+    switch (true) {
+    case interaction.isChatInputCommand(): {
+      execute = command.execute
+      type = "Command"
+      break
     }
 
-    if (interaction.isButton()) {
-      try {await command.button(interaction)}
-      catch (err) {
-        console.error(err)
-        rep(interaction, { content: "This interaction failed (without the red coloring :D) [Button Error]", ephemeral: true })
-      }
+    case interaction.isButton(): {
+      execute = command.button
+      type = "Button"
+      break
     }
 
-    if (interaction.isSelectMenu()) {
-      try {await command.selectMenu(interaction)}
-      catch (err) {
-        console.error(err)
-        rep(interaction, { content: "This interaction failed (without the red coloring :D) [SelectMenu Error]", ephemeral: true })
-      }
+    case interaction.isSelectMenu(): {
+      execute = command.selectMenu
+      type = "SelectMenu"
+      break
     }
 
-    if (interaction.isContextMenuCommand()) {
-      try {await command.ctxMenu(interaction)}
-      catch (err) {
-        console.error(err)
-        rep(interaction, { content: "This interaction failed (without the red coloring :D) [CtxMenu Error]", ephemeral: true })
-      }
+    case interaction.isContextMenuCommand(): {
+      execute = command.ctxMenu
+      type = "CtxMenu"
+      break
     }
 
-    if (interaction.isAutocomplete()) {
-      try {await command.autocomplete(interaction)}
-      catch (err) {
-        console.error(err)
-        rep(interaction, { content: "This interaction failed (without the red coloring :D) [Autocomplete Error]", ephemeral: true })
-      }
+    case interaction.isAutocomplete(): {
+      execute = command.autocomplete
+      type = "Autocomplete"
+      break
     }
-  },
+    }
+
+    await execute(interaction)
+      .catch(err => {
+        console.error(err)
+        rep(interaction, { content: `${emojis.crossmark.shorthand} This interaction failed [${type} Error]`, ephemeral: true })
+      })
+  }
 }
