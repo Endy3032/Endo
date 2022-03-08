475f74b8bb48860bfc92105577be82e7da5f59d7
Endy3032 <61771609+Endy3032@users.noreply.github.com>
2022-03-08 17:53:08 +0700

    fixed http logging

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = express()
 const port = 3032
 
 var httplog = createWriteStream("./logs/http.log", { flags: "a" })
-server.use(morgan("[:date[web]] \":method :url\" :status :user-agent", { stream: os.hostname().includes("local") ? null : httplog }))
+server.use(morgan("[:date[web]] \":method :url\" :status :user-agent", { stream: os.hostname().includes("local") ? httplog : null }))
 
 server.all("/", (_, res) => {
   res.send("Website coming in the future\nStay tuned")
