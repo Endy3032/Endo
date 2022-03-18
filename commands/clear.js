9bf7a77fd511ef0691fb77c7ae18861b9e8466bf
Endy3032 <61771609+Endy3032@users.noreply.github.com>
2022-03-18 20:54:47 +0700

    added clear options

--- commands/clear.js
+++ commands/clear.js
@@ -15,8 +15,26 @@ module.exports = {
         required: true,
       },
       {
-
-      }
+        name: "option",
+        description: "Option to filter out cleared messages",
+        type: ApplicationCommandOptionType.String,
+        choices: [
+          { name: "Bots Only", value: "bots" },
+          { name: "Users Only", value: "users" },
+          // { name: "Texts Only", value: "texts" },
+          // { name: "Mentions Only", value: "mentions" },
+          // { name: "Links Only", value: "links" },
+          // { name: "Embeds Only", value: "embeds" },
+          // { name: "Attachments Only", value: "attachments" },
+        ],
+        required: false,
+      },
+      {
+        name: "user",
+        description: "The user to clear messages from",
+        type: ApplicationCommandOptionType.User,
+        required: false,
+      },
     ]
   },
 
@@ -28,26 +46,30 @@ module.exports = {
 
     if (manageMsgs && manageGuild) {
       const amount = interaction.options.getInteger("amount")
+      const option = interaction.options.getString("option")
+      const user = interaction.options.getUser("user")
+      content = `Press \`Confirm\` to delete \`${amount > 100 ? 100 : amount}\` messages.`
+      if (option && user) {
+        content += `\n**Option & User:** ${option}, ${user}`
+      } else {
+        content += option ? `\n**Option:** ${option}` : user ? `\n**User:** ${user}` : ""
+      }
 
-      components = [
-        {
-          type: ComponentType.ActionRow,
-          components: [
-            {
-              type: ComponentType.Button,
-              style: ButtonStyle.Danger,
-              label: "Confirm",
-              emoji: {
-                name: "trash",
-                id: emojis.trash.id
-              },
-              custom_id: `${amount <= 100 ? amount : 100}`
-            }
-          ]
-        }
-      ]
-
-      interaction.reply({ content: `Press \`Confirm\` to delete \`${amount > 100 ? 100 : amount}\` messages or \`Dismiss message\` to cancel.`, components: components, ephemeral: true })
+      interaction.reply({ content: content, components: [{
+        type: ComponentType.ActionRow,
+        components: [
+          {
+            type: ComponentType.Button,
+            style: ButtonStyle.Danger,
+            label: "Confirm",
+            emoji: {
+              name: "trash",
+              id: emojis.trash.id
+            },
+            custom_id: `${amount <= 100 ? amount : 100}_${option || "none"}_${user?.id || "none"}`
+          }
+        ]
+      }], ephemeral: true })
     } else {
       await interaction.reply({ content: `Permissions needed to use the \`clear\` command:\n${manageMsgs ? emojis.checkmark.shorthand : emojis.crossmark.shorthand} \`Manage Messages\`\n${manageGuild ? emojis.checkmark.shorthand : emojis.crossmark.shorthand} \`Manage Server\``, ephemeral: true })
       console.botLog(`${nordChalk.yellow("Permissions")} [ ${manageMsgs ? nordChalk.green("Manage Messages") : nordChalk.red("Manage Messages")} | ${manageGuild ? nordChalk.green("Manage Server") : nordChalk.red("Manage Server")} ]`, "WARN")
@@ -55,10 +77,25 @@ module.exports = {
   },
 
   async button(interaction) {
-    amount = interaction.customId
+    customID = interaction.customId
+    const [amount, option, user] = customID.split("_")
+    await interaction.deferUpdate()
 
-    await interaction.channel.bulkDelete(amount)
-      .then(console.botLog(`Cleared ${amount} messages`), await interaction.deferUpdate())
-      .catch(console.error)
+    if (option == "none" && user == "none") {
+      await interaction.channel.bulkDelete(amount, true)
+        .then(console.botLog(`Cleared ${amount} messages`))
+        .catch(console.error)
+    } else {
+      messages = await interaction.channel.messages.fetch({ limit: amount })
+      if (option != "none") {
+        option == "bots"
+          ? clearList = messages.filter(message => message.author.bot)
+          : clearList = messages.filter(message => !message.author.bot)
+      } else if (user != "none") {
+        clearList = messages.filter(message => message.author.id == user)
+      }
+
+      await interaction.channel.bulkDelete(clearList)
+    }
   }
 }
