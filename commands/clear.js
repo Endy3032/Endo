ded1edcc161b9d6e80f3aa0c9f9aa835822b9029
Endy3032 <61771609+Endy3032@users.noreply.github.com>
2022-03-19 22:44:48 +0700

    creating text channels & categories

--- commands/clear.js
+++ commands/clear.js
@@ -44,36 +44,36 @@ module.exports = {
     const manageMsgs = interaction.member.permissions.has(PermissionFlagsBits.ManageMessages)
     const manageGuild = interaction.member.permissions.has(PermissionFlagsBits.ManageGuild)
 
-    if (manageMsgs && manageGuild) {
-      const amount = interaction.options.getInteger("amount")
-      const option = interaction.options.getString("option")
-      const user = interaction.options.getUser("user")
-      content = `Press \`Confirm\` to delete \`${amount > 100 ? 100 : amount}\` messages.`
-      if (option && user) {
-        content += `\n**Option & User:** ${option}, ${user}`
-      } else {
-        content += option ? `\n**Option:** ${option}` : user ? `\n**User:** ${user}` : ""
-      }
-
-      interaction.reply({ content: content, components: [{
-        type: ComponentType.ActionRow,
-        components: [
-          {
-            type: ComponentType.Button,
-            style: ButtonStyle.Danger,
-            label: "Confirm",
-            emoji: {
-              name: "trash",
-              id: emojis.trash.id
-            },
-            custom_id: `${amount <= 100 ? amount : 100}_${option || "none"}_${user?.id || "none"}`
-          }
-        ]
-      }], ephemeral: true })
-    } else {
-      await interaction.reply({ content: `Permissions needed to use the \`clear\` command:\n${manageMsgs ? emojis.checkmark.shorthand : emojis.crossmark.shorthand} \`Manage Messages\`\n${manageGuild ? emojis.checkmark.shorthand : emojis.crossmark.shorthand} \`Manage Server\``, ephemeral: true })
+    if (!manageMsgs && !manageGuild) {
       console.botLog(`${nordChalk.yellow("Permissions")} [ ${manageMsgs ? nordChalk.green("Manage Messages") : nordChalk.red("Manage Messages")} | ${manageGuild ? nordChalk.green("Manage Server") : nordChalk.red("Manage Server")} ]`, "WARN")
+      return await interaction.reply({ content: `Permissions needed to use the \`clear\` command:\n${manageMsgs ? emojis.checkmark.shorthand : emojis.crossmark.shorthand} \`Manage Messages\`\n${manageGuild ? emojis.checkmark.shorthand : emojis.crossmark.shorthand} \`Manage Server\``, ephemeral: true })
     }
+
+    const amount = interaction.options.getInteger("amount")
+    const option = interaction.options.getString("option")
+    const user = interaction.options.getUser("user")
+    content = `Press \`Confirm\` to delete \`${amount > 100 ? 100 : amount}\` messages.`
+    if (option && user) {
+      content += `\n**Option & User:** ${option}, ${user}`
+    } else {
+      content += option ? `\n**Option:** ${option}` : user ? `\n**User:** ${user}` : ""
+    }
+
+    interaction.reply({ content: content, components: [{
+      type: ComponentType.ActionRow,
+      components: [
+        {
+          type: ComponentType.Button,
+          style: ButtonStyle.Danger,
+          label: "Confirm",
+          emoji: {
+            name: "trash",
+            id: emojis.trash.id
+          },
+          custom_id: `${amount <= 100 ? amount : 100}_${option || "none"}_${user?.id || "none"}`
+        }
+      ]
+    }], ephemeral: true })
   },
 
   async button(interaction) {
