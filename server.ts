0b13ea63070c24d000472ec36bc7e196e7f0bcd3
Endy3032 <61771609+Endy3032@users.noreply.github.com>
2022-04-01 19:15:29 +0700

    updated server

--- server.ts
+++ server.ts
@@ -3,17 +3,15 @@ import morgan from "morgan"
 import express from "express"
 import { createWriteStream, existsSync, mkdirSync } from "fs"
 
-const server = express()
 const port = 3032
+const server = express()
 
 if (!existsSync("./Logs")) {mkdirSync("./Logs")}
 var networkLog = !os.hostname().includes("local") ? createWriteStream("./Logs/network.log", { flags: "a" }) : null
 server.use(morgan("[:date[web]] \":method :url\" :status :user-agent", { stream: networkLog }))
 
-server.all("/", (_, res) => {
-  res.send("Website coming in the future\nStay tuned")
-})
+server.all("/", (_, res) => res.sendStatus(200))
 
 export default function keepAlive() {
-  server.listen(port, () => {console.tagLog("Server", "Ready")})
+  server.listen(port, () => console.tagLog("Server", "Ready"))
 }
