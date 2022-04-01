6be6bf7614fe356b660eba10c7a24ec1851e72d3
Endy3032 <61771609+Endy3032@users.noreply.github.com>
2022-04-01 19:18:40 +0700

    updated server response log

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ const server = express()
 
 if (!existsSync("./Logs")) {mkdirSync("./Logs")}
 var networkLog = !os.hostname().includes("local") ? createWriteStream("./Logs/network.log", { flags: "a" }) : null
-server.use(morgan("[:date[web]] \":method :url\" :status :user-agent", { stream: networkLog }))
+server.use(morgan("[:date[web]] \":method\" :user-agent", { stream: networkLog }))
 
 server.all("/", (_, res) => res.sendStatus(200))
 
