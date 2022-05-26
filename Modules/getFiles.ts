2d858b7dd11f6c766cb15d94d59b3e0ee311169a
Endy3032 <61771609+Endy3032@users.noreply.github.com>
2022-05-26 13:41:46 +0700

    Minor fix to `getFiles`

--- Modules/getFiles.ts
+++ Modules/getFiles.ts
@@ -1,6 +1,6 @@
 export const getFiles = (dir: string, fileTypes: string[] | string | "folders" = "ts") => {
   const regex = new RegExp(`([.](${(typeof fileTypes == "string" ? [fileTypes] : [...fileTypes]).join("|")}))$`)
   return [...Deno.readDirSync(dir)]
-    .filter(entry => fileTypes == "folders" ? entry.isDirectory : regex.test(entry.name))
+    .filter(entry => fileTypes == "folders" ? entry.isDirectory : entry.isFile && regex.test(entry.name))
     .map(entry => entry.name)
 }
