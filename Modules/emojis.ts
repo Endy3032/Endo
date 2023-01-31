da4a2532e6c80c717acf13ab39ac6114c7b3b146
Endy3032 <endy@endy3032.ml>
2023-01-31 16:06:42 +0700

    Update `emojis` module

--- Modules/emojis.ts
+++ Modules/emojis.ts
@@ -2,19 +2,19 @@ import { createBot } from "discordeno"
 
 const token = Deno.env.get("DiscordToken")
 if (token === undefined) throw new Error("Missing Token")
+
 const bot = createBot({ token })
 
-const emojiss = (await bot.helpers.getEmojis(Deno.env.get("TestGuild") ?? "")).array()
-	.reduce((map, e) => {
-		// map[e.name ?? ""] = { name: e.name, id: e.id?.toString(), shorthand: `<:${e.name}:${e.id}>` }
-		map[e.name ?? ""] = e.id?.toString()
-		return map
-	}, {})
+const emojis = Object.fromEntries(new Map(
+	(await bot.helpers.getEmojis(Deno.env.get("TestGuild") ?? ""))
+		.map(e => [e.name ?? "", e.id?.toString()]),
+))
 
-await Deno.writeTextFile(new URL("./emojis.json", import.meta.url), JSON.stringify(emojiss, null, 2))
+// IntelliSense
+
+Deno.writeTextFileSync(new URL("./emojis.json", import.meta.url), JSON.stringify(emojis, null, "\t"))
 
 import emojisJson from "./emojis.json" assert { type: "json" }
-export const emojis = emojisJson
 
-type emojiName = keyof typeof emojis
-export const shorthand = (name: emojiName) => `<:${name}:${emojis[name]}>`
+export { emojisJson as emojis }
+export const shorthand = (name: keyof typeof emojisJson) => `<:${name}:${emojis[name]}>`
