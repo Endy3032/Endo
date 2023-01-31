32f7819b9afa2e30080c4b638b5de67186880bed
Endy3032 <endy@endy3032.ml>
2023-01-31 16:17:35 +0700

    More minor clean up

--- Events/ready.ts
+++ Events/ready.ts
@@ -1,17 +1,17 @@
 import axiod from "axiod"
 import { IAxiodError, IAxiodResponse } from "axiodInterfaces"
 import { bold, rgb24 } from "colors"
-import { EventHandlers } from "discordeno"
+import { ActivityTypes, EventHandlers } from "discordeno"
 import { activities, Nord, TimeMetric } from "modules"
 
 export const name: keyof EventHandlers = "ready"
 export const main: EventHandlers["ready"] = async (bot, payload) => {
-	const user = `${payload.user.username}#${payload.user.discriminator}`,
-		location = Deno.build.vendor === "unknown" ? "Replit" : "Local",
+	const location = Deno.build.vendor === "unknown" ? "Replit" : "Local",
 		gatewayInfo = `[v${payload.v} | ${bot.gateway.gatewayBot.sessionStartLimit.remaining} Left | ${location}]`
 
-	console.botLog(`${user} ${rgb24(gatewayInfo, Nord.brightYellow)}`, { tag: "Login" })
-	;(function pinger() {
+	console.botLog(`${payload.user.username}#${payload.user.discriminator}} ${rgb24(gatewayInfo, Nord.brightYellow)}`, { tag: "Login" })
+
+	function pinger() {
 		axiod.head(`https://pinger.endy3032.repl.co`)
 			.catch((err: IAxiodError) => {
 				console.botLog(
@@ -24,22 +24,23 @@ export const main: EventHandlers["ready"] = async (bot, payload) => {
 					{ logLevel: "WARN", tag: "pinger" },
 				)
 			})
-		setTimeout(() => pinger(), 5 * TimeMetric.milli_min)
-	})()
+	}
 
 	async function reloadPresence() {
 		const activity = activities()
 		await bot.helpers.editBotStatus(activity)
 		const { type, name, url } = activity.activities[0]
-		const typeString = ["Playing", "Streaming", "Listening to", "Watching"]
 
-		let log = `${typeString[type]} ${name}`
+		let log = `${ActivityTypes[type]}: ${name}`
+
 		if (name === "lofi" && url) {
 			const res = await axiod.get(`https://noembed.com/embed?url=${url.replace("www.youtube.com/watch?v=", "youtu.be/")}`)
 			log += rgb24(`│${res.data.title}\n${rgb24(res.data.url, Nord.brightGreen)}`, Nord.brightCyan)
 		}
-		console.botLog(log, { embed: { description: `[Status] ${typeString[type]} ${name}` }, tag: "Status" })
+
+		console.botLog(log, { embed: { description: `[Status] ${ActivityTypes[type]}: ${name}` }, tag: "Status" })
 	}
 
-	if (Deno.build.vendor === "unknown") setInterval(() => reloadPresence(), 20 * TimeMetric.milli_min)
+	setInterval(pinger, 5 * TimeMetric.milli_min)
+	if (Deno.build.vendor === "unknown") setInterval(reloadPresence, 30 * TimeMetric.milli_min)
 }
